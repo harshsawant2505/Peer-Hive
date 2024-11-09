1ce2d082a46ac85f054b11d61eb91ca843095357
Harsh Sawant <harshsawant2505@gmail.com>
2024-11-09 15:07:41 +0530

    done re 2

---
+++ src/middleware.ts
@@ -0,0 +1,36 @@
+import { NextResponse } from 'next/server'
+import { NextRequest } from 'next/server'
+
+// This function can be marked `async` if using `await` inside
+export function middleware(req: NextRequest) {
+  
+  
+   const token =req.cookies.get('__Secure-next-auth.session-token')?.value || req.cookies.get('next-auth.session-token')?.value;
+
+
+ const path = req.nextUrl.pathname
+
+ const publicPath = path === '/register' || path === '/login' || path == '/landing'
+  
+
+ 
+
+ if (publicPath && token) {
+    return NextResponse.redirect(new URL('/', req.nextUrl))
+ }
+ if (!publicPath && !token) {
+   return NextResponse.redirect(new URL('/landing', req.nextUrl))
+}
+
+}
+
+
+// Add the page paths u need only authenticated people can access
+export const config = {
+  matcher: [
+    
+    '/',
+    '/login',
+   
+    ]
+}
