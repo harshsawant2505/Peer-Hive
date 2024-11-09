3ffc895ae81d7a2bbf3ea72dca199fc4679055c8
Harsh Sawant <harshsawant2505@gmail.com>
2024-11-09 10:21:12 +0530

    bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import Navbar from '@/components/Navbar'
+
 import React from 'react'
 
 function Home() {
   return (
     <div>
 
-    <Navbar />
+    
     <div className='w-full h-screen bg-black text-white flex items-center justify-center'>
       <h1 className='text-4xl '>Home</h1>
       </div>
